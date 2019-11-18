ad3bff479b2443eb6826720dfc6e6b5f23e0bc97
Aaron Williams <aaron.williams@nsc8020l.nscom.lan>
2019-11-18 09:43:23 -0500

    Added input field and html form.

--- MeadStorm.go
+++ MeadStorm.go
@@ -3,21 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"html/template"
 )
 
+// homePage function that provides the first page users visit on the application.
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "MeadStorm Home")
+}
+
+// inputPage function that serves a test page to display and hangle an input field.
+func inputPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("input.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("input:", r.Form["data"])
+	}
+}
+
+// Handler function that handles client requests and provides server's response.
+func handlers() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/input", inputPage)
+	// Listening function that serves web content. Will update to TLS once web application is built.
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
 func main() {
-	handle() // Call the handle function
-	listen() // Call the listen function
-}
-
-func handle() {
-	// Handler function that handles client requests and provides server's response.
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "MeadStorm Test")
-	})
-}
-
-func listen() {
-	// Listening function that serves web content on 443. Will update to TLS once web application is built.
-	http.ListenAndServe(":8080", nil)
+	handlers() // Call the handlers function
 }

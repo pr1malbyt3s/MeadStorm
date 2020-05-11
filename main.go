7773d7ade5c7b378e8a18d0d8a8ebcae99def6c6
pr1malbyt3s <aaronwilliams.infsosec@gmail.com>
2020-05-11 17:06:08 -0400

    CSS Updates

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 
 // Handler function that handles client requests and provides server's response.
 func handlers() {
+	http.Handle("/stylesheets/main.css", http.StripPrefix("/stylesheets/main.css", http.FileServer(http.Dir("./assets/stylesheets/main.css"))))
 	// Handler for home page.
 	http.HandleFunc("/", Home)
 	// Handler for contact page.
 	http.HandleFunc("/contact", ContactPage)
-    // Handler for login page.
-    http.HandleFunc("/login", LoginPage)
+    	// Handler for login page.
+    	http.HandleFunc("/login", LoginPage)
 	// Listening function that serves web content. Will update to TLS once web application is built.
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
